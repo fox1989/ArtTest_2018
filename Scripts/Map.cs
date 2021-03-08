6bcee807fdf49d5d7150aaf2944c75fa94df23f8
fox <610798770@qq.com>
2021-03-08 18:56:41 +0800

    mapEditor

--- Scripts/Map.cs
+++ Scripts/Map.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Map : MonoBehaviour
 {
@@ -8,14 +9,102 @@ public class Map : MonoBehaviour
     GridLayout grid;
 
     public GameObject cubePrefab;
+    public GameObject solpePrefab;
     public GameObject quad;
 
+    public GameObject currSelectGo = null;
+
+    public Material selectMat;
+    public Material normalMat;
+
+
+    private GameObject currPrefab;
+
+
+    public GameObject cubeTypes;
+
+    public GameObject brushTypes;
+
+
+    public enum BrushType
+    {
+        Add,//添加
+        Dle,//删掉
+        Select,//选择
+    }
+
+
+    public enum CubeType
+    {
+        Cube,
+        Slope,
+    }
+
+
+    public BrushType brushType;
+
+
+    public CubeType cubeType;
 
     // Start is called before the first frame update
     void Start()
     {
-
         grid = GetComponent<GridLayout>();
+
+
+        InitCubeType();
+        InitBrushType();
+    }
+
+
+    void InitCubeType()
+    {
+
+        Toggle[] cubeToggles = cubeTypes.GetComponentsInChildren<Toggle>();
+
+        foreach (var item in cubeToggles)
+        {
+            item.onValueChanged.AddListener((bool b) =>
+            {
+                if (b)
+                {
+                    cubeType = (CubeType)System.Enum.Parse(typeof(CubeType), item.name);
+                    SetCubeType();
+                }
+            });
+        }
+
+    }
+
+    void InitBrushType()
+    {
+        Toggle[] Toggles = brushTypes.GetComponentsInChildren<Toggle>();
+        foreach (var item in Toggles)
+        {
+            item.onValueChanged.AddListener((bool b) =>
+            {
+                if (b)
+                {
+                    brushType = (BrushType)System.Enum.Parse(typeof(BrushType), item.name);
+                }
+            });
+        }
+    }
+
+
+    public void SetCubeType()
+    {
+        switch (cubeType)
+        {
+            case CubeType.Cube:
+                currPrefab = cubePrefab;
+                break;
+            case CubeType.Slope:
+                currPrefab = solpePrefab;
+                break;
+            default:
+                break;
+        }
 
     }
 
@@ -35,6 +124,8 @@ public class Map : MonoBehaviour
             quad.transform.position = setPos;
             Vector3 center = WorldToCenter(hit.point);
             quad.transform.LookAt(center);
+
+
         }
         else
         {
@@ -42,25 +133,81 @@ public class Map : MonoBehaviour
         }
 
 
-
         if (Input.GetMouseButtonDown(0))
         {
             if (raycast)
             {
-                GameObject cube = Instantiate(cubePrefab, transform);
-                cube.transform.position = WorldToCenter(hit.point);
-                cube.SetActive(true);
+
+                switch (brushType)
+                {
+                    case BrushType.Add:
+                        Add(hit.point);
+                        break;
+                    case BrushType.Dle:
+                        Dle(hit);
+                        break;
+                    case BrushType.Select:
+                        Select(hit);
+                        break;
+                    default:
+                        break;
+                }
+
             }
         }
 
 
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Rotate90(-1);
+        }
 
 
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Rotate90(1);
 
-
+        }
     }
 
 
+
+    void Rotate90(int dir)
+    {
+        if (currSelectGo != null)
+        {
+
+            Vector3 angle = currSelectGo.transform.localEulerAngles;
+            angle.y += 90 * dir;
+            currSelectGo.transform.localEulerAngles = angle;
+        }
+
+    }
+
+    void Add(Vector3 hitPoint)
+    {
+        GameObject cube = Instantiate(currPrefab, transform);
+        cube.transform.position = WorldToCenter(hitPoint);
+        cube.SetActive(true);
+        SelectGo(cube);
+    }
+
+
+    void Dle(RaycastHit hit)
+    {
+        if (hit.collider.gameObject.tag == "cube")
+        {
+            Destroy(hit.collider.gameObject);
+        }
+    }
+
+    private void Select(RaycastHit hit)
+    {
+        if (hit.collider.gameObject.tag == "cube")
+        {
+            SelectGo(hit.collider.gameObject);
+        }
+    }
 
     Vector3[] dirs = new Vector3[]
      {
@@ -93,6 +240,20 @@ public class Map : MonoBehaviour
     }
 
 
+    void SelectGo(GameObject go)
+    {
+
+        go.GetComponent<Renderer>().material = selectMat;
+
+        if (currSelectGo != null)
+        {
+            currSelectGo.GetComponent<Renderer>().material = normalMat;
+
+        }
+        currSelectGo = go;
+
+    }
+
 
     Vector3 CellToCenter(Vector3Int cell)
     {
@@ -104,5 +265,6 @@ public class Map : MonoBehaviour
     {
         return CellToCenter(grid.WorldToCell(world));
     }
+
 
 }
